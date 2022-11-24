633232e68451f8e46c83a23199bd2321efd528db
Resalat <resalatislam5@gmail.com>
2022-11-24 23:37:16 +0600

    Shop Electric Bikes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -13,11 +13,23 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function  run() {
     try{
         const productCollection = client.db('SecondTimeBuy').collection('Products')
-        
         app.get('/products',async(req,res)=>{
             const query = {};
             const products = await productCollection.find(query).toArray()
             res.send(products)
+        })
+        app.get('/products/:id',async(req,res)=>{
+            const id = req.params.id;
+            console.log(id)
+            const query = {_id : ObjectId(id)};
+            const product = await productCollection.findOne(query)
+            res.send(product)
+        })
+        app.get('/electirc-bikes',async(req,res)=>{
+            const query = {'categoryname' : 'Electirc Bikes'};
+            const electircBike = await productCollection.find(query).toArray()
+            console.log(electircBike)
+            res.send(electircBike)
         })
     }
     finally{
