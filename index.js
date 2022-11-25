42bcf533cfa43561c8e32acf680ce3bd5cb46eb0
Resalat <resalatislam5@gmail.com>
2022-11-26 03:12:52 +0600

    my products delete

--- index.js
+++ index.js
@@ -109,6 +109,20 @@ async function  run() {
                 res.send({result,role})
             }
         })
+        // My Product
+        app.get('/myproducts',async (req,res)=>{
+            const email = req.query.email;
+            const query = {email : email}
+            const result = await productCollection.find(query).toArray();
+            res.send(result)
+        })
+        // My Product delete
+        app.delete('/myproduct/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)}
+            const result = await productCollection.deleteOne(query)
+            res.send(result)
+        })
     }
     finally{
 
